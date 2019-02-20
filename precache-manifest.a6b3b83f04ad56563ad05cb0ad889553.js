fd35e81a3fe8b2dc8486d201faae21aeb3fe32c5
Abdullah <ab4ullahkh@gmail.com>
2019-02-20 16:06:09 +0300

    Updates

---
+++ precache-manifest.a6b3b83f04ad56563ad05cb0ad889553.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe0938438911aad2160d",
+    "url": "/project-2/static/css/main.8cf907b2.chunk.css"
+  },
+  {
+    "revision": "fe0938438911aad2160d",
+    "url": "/project-2/static/js/main.fe093843.chunk.js"
+  },
+  {
+    "revision": "8204681cae27f55d9384",
+    "url": "/project-2/static/js/runtime~main.8204681c.js"
+  },
+  {
+    "revision": "2f55b2f9031b8e5310dc",
+    "url": "/project-2/static/js/2.2f55b2f9.chunk.js"
+  },
+  {
+    "revision": "65783754eaf673b0b287b7421c769dc0",
+    "url": "/project-2/static/media/r.65783754.jpg"
+  },
+  {
+    "revision": "00ebfc09e9b2cf3b0f6797acce9d48c4",
+    "url": "/project-2/index.html"
+  }
+];
